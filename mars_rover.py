072bc8f5977f9a950f8d4429fbd4a2671434d48d
Jim Doepp <jimd@libraryideas.com>
2020-04-08 21:55:43 -0400

    Added plateau

--- mars_rover.py
+++ mars_rover.py
@@ -22,8 +22,31 @@ class Directions:
         self.s.left, self.s.right = (self.e, self.w)
         self.e.left, self.e.right = (self.s, self.n)
         self.w.left, self.w.right = (self.n, self.s)
+
     def get_node_from_val(self, value):
         return self.nodes[value]
+
+class Plateau:
+    '''A matrix (list of lists). The origin of the plateau is at the lower left corner. This requires the numberst to be transposed'''
+    def __init__(self, top_right):
+        self.grid = [[0] * (top_right[0] + 1)] * (top_right[1] + 1)
+        self.size = len(self.grid[0]), len(self.grid)
+
+    def transpose(self, x, y, reverse=False):
+        '''Transpose from list of lists (row, column) to plateau (with (0,0) in bottom left corner). reverse=True will transpose the from plateau to lists'''
+        if not reverse:
+            if x > self.size[0] - 1:
+                return None
+            return self.size[1] - 1 - y, x
+        else:
+            if y > self.size[0] - 1:
+                return None
+            return y, self.size[0] - 1 - x
+
+    def set_value(self, x, y, value):
+        print(x,y,value)
+        self.grid[x][y] = value
+    
 
 def main():
     pass
--- tests.py
+++ tests.py
@@ -41,8 +41,71 @@ class DirectionsTests(unittest.TestCase):
         east_node = self.directions.get_node_from_val('E')
         self.assertEqual(east_node.direction, 'E')
 
+class PlateauTests(unittest.TestCase):
+    '''Test the laying out of the matrix and transposition'''
+    def setUp(self):
+        self.plateau_2x4 = mars_rover.Plateau([1,3])
+        self.plateau_4x4 = mars_rover.Plateau([3,3])
+        self.plateau_6x6 = mars_rover.Plateau([5,5])
 
+        self.assertEqual(self.plateau_4x4.size, (4,4))
+        self.assertEqual(self.plateau_6x6.size, (6,6))
+        self.assertEqual(self.plateau_2x4.size, (2,4))
+    
+    def test_transpose(self):
+        '''
+        A 4x4 grid transposed: plateau to matrix grid
+             (0,3) (1,3) (2,3) (3,3)      (0,0) (0,1) (0,2) (0,3)
+             (0,2) (1,2) (2,2) (3,2) ==>  (1,0) (1,1) (1,2) (1,3)       
+             (0,1) (1,1) (2,1) (3,1)      (2,0) (2,1) (2,2) (2,3)       
+             (0,0) (1,0) (2,0) (3,0) <==  (3,0) (3,1) (3,2) (3,3)  
+             (x,y)                  reverse  
+
+        2x4 grid
+            (0,3) (1,3)     (0,0) (0,1)
+            (0,2) (1,2)     (1,0) (1,1)
+            (0,1) (1,1)     (2,0) (2,1)
+            (0,0) (1,0)     (3,0) (3,1)
+        '''
+        location = [0, 0]
+        new_location = self.plateau_6x6.transpose(*location)
+        self.assertEqual(new_location, (5,0))
+
+        new_location = self.plateau_4x4.transpose(*location)
+        self.assertEqual(new_location, (3,0))
+
+        new_location = self.plateau_2x4.transpose(*location)
+        self.assertEqual(new_location, (3,0))
+
+        new_location = self.plateau_4x4.transpose(*location, reverse=True)
+        self.assertEqual(new_location, (0,3))
+
+        location = [1, 2]
+        new_location = self.plateau_6x6.transpose(*location)
+        self.assertEqual(new_location, (3,1))
+
+        new_location = self.plateau_4x4.transpose(*location)
+        self.assertEqual(new_location, (1,1))
+
+        new_location = self.plateau_2x4.transpose(*location)
+        self.assertEqual(new_location, (1,1))
         
+        new_location = self.plateau_6x6.transpose(*location, reverse=True)
+        self.assertEqual(new_location, (2,4))
+
+        new_location = self.plateau_4x4.transpose(*location, reverse=True)
+        self.assertEqual(new_location, (2,2))
+
+        new_location = self.plateau_2x4.transpose(*location, reverse=True)
+        self.assertEqual(new_location, None)
+
+        location = [2,0]
+        new_location = self.plateau_2x4.transpose(*location)
+        self.assertEqual(new_location, None)
+
+    def test_set_value(self):
+        self.plateau_6x6.set_value(3,3, 1)
+        self.assertEqual(self.plateau_6x6.grid[3][3], 1)
 
 if __name__ == "__main__":
     unittest.main()
